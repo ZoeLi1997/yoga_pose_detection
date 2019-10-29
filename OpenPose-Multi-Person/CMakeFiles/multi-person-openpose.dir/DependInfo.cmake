b4299d959a98e8fd1e2696abfe78ca5dd32e65dd
ZoeLi1997 <zli32@calpoly.edu>
2019-10-29 16:26:04 -0700

    first commit

---
+++ OpenPose-Multi-Person/CMakeFiles/multi-person-openpose.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/lizhengzheng/Desktop/480/project/learnopencv/OpenPose-Multi-Person/multi-person-openpose.cpp" "/Users/lizhengzheng/Desktop/480/project/learnopencv/OpenPose-Multi-Person/CMakeFiles/multi-person-openpose.dir/multi-person-openpose.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/Cellar/opencv/4.1.2/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ OpenPose-Multi-Person/CMakeFiles/multi-person-openpose.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/multi-person-openpose.dir/multi-person-openpose.cpp.o"
+  "multi-person-openpose"
+  "multi-person-openpose.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/multi-person-openpose.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
